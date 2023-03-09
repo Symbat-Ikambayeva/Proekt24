4c8665d1c6535bd7522b12dab2aca160d2d979ca
Symbat-Ikambayeva <sumbat2307@mail.ru>
2023-03-09 21:08:21 +0600

    работа с файлами ресурсов; использование пакета java.util.logging; обработка исключений.

--- src/main/java/Boot.java
+++ src/main/java/Boot.java
@@ -9,28 +9,44 @@ import xlsreader.XlsWriter;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class Boot {
+
+    private static final Logger logger = Logger.getLogger(Boot.class.getName());
+
     public static void main(String[] args) throws IOException {
+
+        try {
+            LogManager.getLogManager().readConfiguration(
+                    Boot.class.getResourceAsStream("/logging.properties"));
+        } catch (IOException e) {
+            System.err.println("Could not setup logger configuration: " + e.toString());
+        }
+
+        logger.log(Level.INFO, "Application started, Logger configured");
+
 
         List<University> universities =
                 XlsReader.readXlsUniversities("src/main/resources/universityInfo.xlsx");
         UniversityComparator universityComparator= ComparatorUtil.getUniversityComparator(UniversityType.YEAR_OF_FOUNDATION);
         universities.sort(universityComparator);
-        String universitiesJson = JsonUtil.universityListToJson(universities);
-        System.out.println(universitiesJson);
+       /* String universitiesJson = JsonUtil.universityListToJson(universities);
+        System.out.println(universitiesJson);*/
         /*universities.stream().sorted(universityComparator).forEach(System.out::println);
       for(University university : universities) {
             System.out.println(university);
         }*/
-        universities.forEach(university -> {
+       /* universities.forEach(university -> {
             String universityJson = JsonUtil.universityToJson(university);
             System.out.println(universityJson);
             University universityFromJson = JsonUtil.jsonToUniversity(universityJson);
             System.out.println(universityFromJson);
         });
         List<University> universitiesFromJson = JsonUtil.jsonToUniversityList(universitiesJson);
-        System.out.println("Результат сравнения: "+ (universities.size() == universitiesFromJson.size()));
+        System.out.println("Результат сравнения: "+ (universities.size() == universitiesFromJson.size()));*/
 
         List<Student> students =
                 XlsReader.readXlsStudents("src/main/resources/universityInfo.xlsx");
@@ -41,17 +57,18 @@ public class Boot {
         }*/
         students.sort(studentComparator);
         String studentsJson = JsonUtil.studentListToJson(students);
-        System.out.println(studentsJson);
+       /* System.out.println(studentsJson);
         students.forEach(student -> {
             String studentJson = JsonUtil.studentToJson(student);
             System.out.println(studentJson);
             Student studentFromJson = JsonUtil.jsonToStudent(studentJson);
             System.out.println(studentFromJson);
-        });
-        List<Student> studentsFromJson = JsonUtil.jsonToStudentList(studentsJson);
-        System.out.println("Результат сравнения: "+ (students.size() == studentsFromJson.size()));
+        });*/
+        //List<Student> studentsFromJson = JsonUtil.jsonToStudentList(studentsJson);
+        //System.out.println("Результат сравнения: "+ (students.size() == studentsFromJson.size()));
 
         List<Statistics> statisticsList = StatisticsUtil.createStatistics(students, universities);
         XlsWriter.writeXlsStatistics(statisticsList, "src/main/resources/Statistics.xlsx");
+        logger.log(Level.INFO, "Application finished");
     }
 }
--- src/main/java/model/StatisticsUtil.java
+++ src/main/java/model/StatisticsUtil.java
@@ -9,15 +9,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class StatisticsUtil {
+
+    private static final Logger logger = Logger.getLogger(StatisticsUtil.class.getName());
 
     private StatisticsUtil() {
     }
 
     public static List<Statistics> createStatistics(List<Student> students,
                                                     List<University> universities) {
+
+        logger.log(Level.INFO, "Statistics model started");
 
         List<Statistics> statisticsList = new ArrayList<>();
 
@@ -52,7 +58,7 @@ public class StatisticsUtil {
             avgExamScore.ifPresent(value -> statistics.setAvgExamScore(
                     (float) BigDecimal.valueOf(value).setScale(2, RoundingMode.HALF_UP).doubleValue()));
         });
-
+        logger.log(Level.INFO, String.format("Statistics model finished with %s statistical objects", statisticsList.size()));
         return statisticsList;
     }
 }
